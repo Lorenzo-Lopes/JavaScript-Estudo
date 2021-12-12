283bcd08ea1f7e0b4e252dd39896729a507eab74
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2021-12-11 21:25:30 -0300

    aula 1 curso de JS

---
+++ Primeiros-passos-com-a-linguagem/001.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com variaveis! 001");
+
+const idade = 23;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade*2);
+console.log(idade/2);
+
+
+
---
+++ Primeiros-passos-com-a-linguagem/002.js
@@ -0,0 +1,12 @@
+//trabalhando com variaveis
+console.log("Trabalhando com variaveis! 002");
+
+//JS é case sensitive
+
+const idade=29;
+const nome  ="Lorenzo";
+let ano =2021;
+
+//Idade= 30;
+
+console.log(Idade);
