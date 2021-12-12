d051965e538a64c3cfea941e3044cfce3e6b78d4
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2021-12-11 21:25:44 -0300

    .

---
+++ Primeiros-passos-com-a-linguagem/005.js
@@ -0,0 +1,15 @@
+console.log("Atribuicao de Variaveis");
+
+const idade=29;
+let nome  ="Lorenzo";
+const sobrenome= "Lopes";
+let ano =2021;
+
+console.log(nome+sobrenome);//sem espaço entre os nomes
+console.log(nome, sobrenome);//com espaço
+console.log(nome + " " + sobrenome);//com espaço
+
+console.log(`Meu nome é ${nome}`);
+
+nome= nome+sobrenome;
+console.log(`Meu nome é ${nome}`);
