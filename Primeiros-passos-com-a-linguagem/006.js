d0cafde3559d480a68020b5c10fce8e8ab3acfb2
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2021-12-13 15:11:09 -0300

    aula3 -- Listas
    Criação de Arrays;
    
    Inserção de itens em um array;
    
    Array splice;
    
    Palavras reservadas;
    
    Indices do array;

---
+++ Primeiros-passos-com-a-linguagem/006.js
@@ -0,0 +1,26 @@
+console.log(`Trabalhando com listas`)
+
+// const salvador=`Salvador`
+// const saoPaulo=`Sao Paulo`
+// const rioDeJanaiero= `Rio de janeiro`
+
+// console.log(`Destinos possiveis `)
+// console.log( salvador, saoPaulo ,rioDeJanaiero)
+
+
+const listaDeDestinos = new Array(
+    `Salvador`, 
+    `Sao Paulo`, 
+    `Rio de janeiro`
+    
+)
+console.log(listaDeDestinos)
+
+listaDeDestinos.push(`Curitiba`)
+
+console.log(listaDeDestinos)
+listaDeDestinos.pop()
+console.log(listaDeDestinos)
+listaDeDestinos.slice(1,1)
+console.log(listaDeDestinos)
+
